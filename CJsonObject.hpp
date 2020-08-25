cffc656f591abac996aa07e954037def5b475ed4
Shi Zhangkun <53963800+YOYOYOYOGA-seu@users.noreply.github.com>
2020-08-26 00:26:56 +0800

    加入CJsonObject::Override函数
    Replace失败时调用Add创建他，在不知道目标是否存在时免去用户自行判断的操作

--- CJsonObject.hpp
+++ CJsonObject.hpp
@@ -92,7 +92,12 @@ public:     // method of ordinary json object
     bool Replace(const std::string& strKey, float fValue);
     bool Replace(const std::string& strKey, double dValue);
     bool ReplaceWithNull(const std::string& strKey);    // replace value with null
-
+    template <typename T> bool Override(const std::string& strKey,T& value) 
+    {
+      if(Replace(strKey,value) == false)
+        return Add(strKey,value);
+      return true;
+    }
 public:     // method of json array
     int GetArraySize();
     CJsonObject& operator[](unsigned int uiWhich);
